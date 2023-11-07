418fe64c065296261ec50006840e12d212faca69
ming peng <362606310@qq.com>
2023-11-07 14:06:24 +0800

    first commit code

---
+++ BlazingPizza.Client/JSRuntimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.JSInterop;
+
+public static class JSRuntimeExtensions
+{
+    public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message)
+    {
+        return jsRuntime.InvokeAsync<bool>("confirm", message);
+    }
+}
---
+++ BlazingPizza.ComponentsLibrary/wwwroot/pushNotifications.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    // Note: Replace with your own key pair before deploying
+    //const applicationServerPublicKey = 'BKv18GHbItyciD-6uLukak3tDuo1lEp-7tNNvV0HDAlUMtxYmsjbFC9b_lCgPYyJlbuYAotxB7Ve8TmKgqoWYfo'
+    const applicationServerPublicKey = 'BDjoKmYLfquB_YHWiUFrpaol4ToLirbJtFNyZTAqL42J1F4YjuGg6_7CD7Qrmglvf7TAUSyaya3lnvgywWnDnG0';
+    window.blazorPushNotifications = {
+        requestSubscription: async () => {
+            const worker = await navigator.serviceWorker.getRegistration();
+            const existingSubscription = await worker.pushManager.getSubscription();
+            if (!existingSubscription) {
+                const newSubscription = await subscribe(worker);
+                if (newSubscription) {
+                    return {
+                        url: newSubscription.endpoint,
+                        p256dh: arrayBufferToBase64(newSubscription.getKey('p256dh')),
+                        auth: arrayBufferToBase64(newSubscription.getKey('auth'))
+                    };
+                }
+            }
+        }
+    };
+
+    async function subscribe(worker) {
+        try {
+            return await worker.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: applicationServerPublicKey
+            });
+        } catch (error) {
+            if (error.name === 'NotAllowedError') {
+                return null;
+            }
+            throw error;
+        }
+    }
+
+    function arrayBufferToBase64(buffer) {
+        // https://stackoverflow.com/a/9458996
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+})();
